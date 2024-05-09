0ed1fa7fa5cbf8afd6f5428d867a02c0560433f4
baharkose <baharkse17@gmail.com>
2024-05-09 16:46:00 +0300

    feat: add category model

---
+++ src/models/category.js
@@ -0,0 +1,24 @@
+"use strict";
+/* -------------------------------------------------------
+    NODEJS EXPRESS | STOCK API
+------------------------------------------------------- */
+
+// CATEGORY MODEL
+const mongoose = require("../configs/dbConnection");
+
+const CategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+    collection: "categories",
+  }
+);
+
+module.exports = mongoose.model("Category", CategorySchema);
