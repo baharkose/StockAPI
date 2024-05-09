5cac9fdcbdb82722e041d19cebb9cb390c017d33
baharkose <baharkse17@gmail.com>
2024-05-09 16:04:04 +0300

    fix: index router

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@
 ------------------------------------------------------- */
 const router = require('express').Router()
 /* ------------------------------------------------------- */
-// ROUTER INDEX:
+// routes/:
 
 // URL: /
 
@@ -15,10 +15,19 @@ router.use('/users', require('./user'))
 // token:
 router.use('/tokens', require('./token'))
 
-// car:
-router.use('/cars', require('./car'))
-// reservation:
-router.use('/reservations', require('./reservation'))
+
+// // brand:
+// router.use('/brands', require('./brand'))
+// // category:
+// router.use('/categories', require('./category'))
+// // firm:
+// router.use('/firms', require('./firm'))
+// // product:
+// router.use('/products', require('./product'))
+// // purchase:
+// router.use('/purchases', require('./purchase'))
+// // sale:
+// router.use('/sales', require('./sale'))
 
 // document:
 router.use('/documents', require('./document'))
