5b8ead718c328779d023dbac7dd7390dcb455c2f
baharkose <baharkse17@gmail.com>
2024-05-12 11:51:13 +0300

    feat: add purchase.router

--- src/routes/purchase.js
+++ src/routes/purchase.js
@@ -3,3 +3,23 @@
 /* -------------------------------------------------------
     NODEJS EXPRESS | STOCK API
 ------------------------------------------------------- */
+// PURCHASE CONTROLLER
+
+// routes/purchase
+const router = require("express").Router();
+const permissions = require("../middlewares/permissions");
+const purchase = require("../controllers/purchase");
+/* ------------------------------------------------------- */
+
+// URL: purchases
+router
+  .route("/")
+  .get(permissions.isStaff, purchase.list)
+  .post(permissions.isStaff, purchase.create);
+router
+  .route("/id")
+  .get(permissions.isLogin, purchase.read)
+  .put(permissions.isStaff, purchase.update)
+  .patch(permissions.isAdmin, purchase.delete);
+/* ------------------------------------------------------- */
+module.exports = router;
