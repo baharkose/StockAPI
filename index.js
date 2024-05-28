7bed17a6b4da5e95b30277b9f795967dfc2469e5
Bahar Kose <110201916+baharkose@users.noreply.github.com>
2024-05-28 23:55:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict"
 /* -------------------------------------------------------
-    NODEJS EXPRESS | CLARUSWAY FullStack Team
+    NODEJS EXPRESS | 
 ------------------------------------------------------- */
 const express = require('express')
 const app = express()
